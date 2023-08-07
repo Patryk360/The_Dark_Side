944b4a12d19e499490f3ab304cb2ef500d6e121f
Patryk360 <nexigamingyt@gmail.com>
2023-08-07 13:14:38 +0200

    0.0.2

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    public Camera firstPersonCamera;
+    public Camera thirdPersonCamera;
+    public Camera airplaneCamera;
+    public GameObject airplane;
+    
     public Transform player;
     public Transform cam;
     public float sensitivity;
@@ -24,18 +29,31 @@ public class PlayerCamera : MonoBehaviour
         player.rotation = Quaternion.Euler(0, x, 0);
         cam.rotation = Quaternion.Euler(-y, x, 0);
         
-        if (Input.GetKey(KeyCode.F3))
-        {
-            
-        }
-        
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             player.localScale = new Vector3(1, 0.5f, 1);
         }
-        if (Input.GetKeyUp(KeyCode.LeftControl))
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             player.localScale = new Vector3(1, 1, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (firstPersonCamera.enabled == true)
+            {
+                firstPersonCamera.enabled = false;
+                thirdPersonCamera.enabled = true;
+            }
+            else if (thirdPersonCamera.enabled == true)
+            {
+                thirdPersonCamera.enabled = false;
+                airplaneCamera.enabled = true;
+            }
+            else if (airplaneCamera.enabled == true)
+            {
+                airplaneCamera.enabled = false;
+                firstPersonCamera.enabled = true;
+            }
         }
     }
 }
